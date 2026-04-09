985a42ea4c9f7bfd3500ec49d8fba03ad4ec571e
Ayrton3712 <mugittty@gmail.com>
2026-04-09 19:03:28 -0300

    Redefined outline, started writing basic p(n)

--- sheldon_prime_b10.py
+++ sheldon_prime_b10.py
@@ -1,8 +1,22 @@
-def mirror(r):
+def get_prime(n):
+    i = 0
+    for j in range(n - 1):
+        if (n == 0):
+            return 1
+        
+        divisor = 2
+        while (divisor * divisor <= n):
+            if (n % divisor == 0):
+                continue
+            divisor += 1
+
+def reverse_digits(r):
     pass
 
-def product(r):
+def multiply_digit(r):
     pass
 
 def is_sheldon(r):
     pass
+
+#get_prime(3)
